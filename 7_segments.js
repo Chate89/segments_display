6dd07615e17bb891f36534d41a43dfc3f8d10e92
Chate89 <gastonchatelet@gmail.com>
2017-05-03 12:34:20 -0300

    Add files via upload

---
+++ 7_segments.js
@@ -0,0 +1,246 @@
+
+var digits = [];
+var separators = [];
+var numSep = 20;
+var numDig = 40;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colON1 = color(200, 30, 0);      // RED
+  colON2 = color(40, 200, 0);      // GREEN
+  colON3 = color(0, 150, 255);      // BLUE
+  colON4 = color(255);             //WHITE
+  colOFF1 = color(25, 0, 0);
+  colOFF2 = color(0, 25, 0);
+  colOFF3 = color(0, 0, 25);
+  colOFF4 = color(20);
+
+    for (var i = 0; i < numDig; i++) {
+      digits[i] = new Digit();
+    }
+    for (var i = 0; i < numSep; i++) {
+      separators[i] = new Separator();
+    }
+}
+
+function draw() {
+  background(0);
+  noStroke();
+
+  // ---------- 1 ---------- //
+  // hour
+  var hou = str(hour());
+  if (hou.length < 2) {
+    hou = "0"+str(hour());
+  } else {
+    hou = str(hour());
+  }
+  for (var i = 0; i < 2; i++) {
+    digits[i].x = 100+(i)*digCan;
+    digits[i].y = 70
+    digits[i].onCol = colON1;
+    digits[i].offCol = colOFF1;
+    digits[i].display();
+    digits[i].val = hou.slice(i, i+1);
+  }
+  // minutero
+  var min = str(minute());
+  if (min.length < 2) {
+    min = "0"+str(minute());
+  } else {
+    min = str(minute());
+  }
+  for (var i = 2; i < 4; i++) {
+    digits[i].x = 104+(i)*digCan;
+    digits[i].y = 70
+    digits[i].onCol = colON1;
+    digits[i].offCol = colOFF1;
+    digits[i].display();
+    digits[i].val = min.slice(i-2, i-1);
+  }
+  // segundero
+  var sec = str(second());
+  if (sec.length < 2) {
+    sec = "0"+str(second());
+  } else {
+    sec = str(second());
+  }
+  for (var i = 4; i < 6; i++) {
+    digits[i].x = 108+(i)*digCan;
+    digits[i].y = 70
+    digits[i].onCol = colON1;
+    digits[i].offCol = colOFF1;
+    digits[i].display();
+    digits[i].val = sec.slice(i-4, i-3);
+  }
+  // seperator
+  for (var i = 0; i < 2; i++) {
+    separators[i].x = 142+i*55;
+    separators[i].y = 70;
+    separators[i].onCol = colON1;
+    separators[i].display();
+  }
+
+  // ---------- 2 ---------- //
+  // hour
+  var hou = str(hour());
+  if (hou.length < 2) {
+    hou = "0"+str(hour());
+  } else {
+    hou = str(hour());
+  }
+  for (var i = 0; i < 2; i++) {
+    digits[i].x = 100+(i)*digCan;
+    digits[i].y = 140
+    digits[i].onCol = colON2;
+    digits[i].offCol = colOFF2;
+    digits[i].val = hou.slice(i, i+1);
+    digits[i].display();
+  }
+  // minutero
+  var min = str(minute());
+  if (min.length < 2) {
+    min = "0"+str(minute());
+  } else {
+    min = str(minute());
+  }
+  for (var i = 2; i < 4; i++) {
+    digits[i].x = 104+(i)*digCan;
+    digits[i].y = 140
+    digits[i].onCol = colON2;
+    digits[i].offCol = colOFF2;
+    digits[i].display();
+    digits[i].val = min.slice(i-2, i-1);
+  }
+  // segundero
+  var sec = str(second());
+  if (sec.length < 2) {
+    sec = "0"+str(second());
+  } else {
+    sec = str(second());
+  }
+  for (var i = 4; i < 6; i++) {
+    digits[i].x = 108+(i)*digCan;
+    digits[i].y = 140
+    digits[i].onCol = colON2;
+    digits[i].offCol = colOFF2;
+    digits[i].display();
+    digits[i].val = sec.slice(i-4, i-3);
+  }
+  // seperator
+  for (var i = 0; i < 2; i++) {
+    separators[i].x = 142+i*55;
+    separators[i].y = 140;
+    separators[i].onCol = colON2;
+    separators[i].display();
+  }
+
+  // ---------- 3 ---------- //
+  // hour
+  var hou = str(hour());
+  if (hou.length < 2) {
+    hou = "0"+str(hour());
+  } else {
+    hou = str(hour());
+  }
+  for (var i = 0; i < 2; i++) {
+    digits[i].x = 100+(i)*digCan;
+    digits[i].y = 210
+    digits[i].onCol = colON3;
+    digits[i].offCol = colOFF3;
+    digits[i].display();
+    digits[i].val = hou.slice(i, i+1);
+  }
+  // minutero
+  var min = str(minute());
+  if (min.length < 2) {
+    min = "0"+str(minute());
+  } else {
+    min = str(minute());
+  }
+  for (var i = 2; i < 4; i++) {
+    digits[i].x = 104+(i)*digCan;
+    digits[i].y = 210
+    digits[i].onCol = colON3;
+    digits[i].offCol = colOFF3;
+    digits[i].display();
+    digits[i].val = min.slice(i-2, i-1);
+  }
+  // segundero
+  var sec = str(second());
+  if (sec.length < 2) {
+    sec = "0"+str(second());
+  } else {
+    sec = str(second());
+  }
+  for (var i = 4; i < 6; i++) {
+    digits[i].x = 108+(i)*digCan;
+    digits[i].y = 210
+    digits[i].onCol = colON3;
+    digits[i].offCol = colOFF3;
+    digits[i].display();
+    digits[i].val = sec.slice(i-4, i-3);
+  }
+  // seperator
+  for (var i = 0; i < 2; i++) {
+    separators[i].x = 142+i*55;
+    separators[i].y = 210;
+    separators[i].onCol = colON3;
+    separators[i].display();
+  }
+
+  // ---------- 4 ---------- //
+  // hour
+  var hou = str(hour());
+  if (hou.length < 2) {
+    hou = "0"+str(hour());
+  } else {
+    hou = str(hour());
+  }
+  for (var i = 0; i < 2; i++) {
+    digits[i].x = 100+(i)*digCan;
+    digits[i].y = 280
+    digits[i].onCol = colON4;
+    digits[i].offCol = colOFF4;
+    digits[i].val = hou.slice(i, i+1);
+    digits[i].display();
+  }
+  // minutero
+  var min = str(minute());
+  if (min.length < 2) {
+    min = "0"+str(minute());
+  } else {
+    min = str(minute());
+  }
+  for (var i = 2; i < 4; i++) {
+    digits[i].x = 104+(i)*digCan;
+    digits[i].y = 280
+    digits[i].onCol = colON4;
+    digits[i].offCol = colOFF4;
+    digits[i].display();
+    digits[i].val = min.slice(i-2, i-1);
+  }
+  // segundero
+  var sec = str(second());
+  if (sec.length < 2) {
+    sec = "0"+str(second());
+  } else {
+    sec = str(second());
+  }
+  for (var i = 4; i < 6; i++) {
+    digits[i].x = 108+(i)*digCan;
+    digits[i].y = 280
+    digits[i].onCol = colON4;
+    digits[i].offCol = colOFF4;
+    digits[i].display();
+    digits[i].val = sec.slice(i-4, i-3);
+  }
+  // seperator
+  for (var i = 0; i < 2; i++) {
+    separators[i].x = 142+i*55;
+    separators[i].y = 280;
+    separators[i].onCol = colON4;
+    separators[i].display();
+  }
+
+}
---
+++ digit.js
@@ -0,0 +1,156 @@
+var digCan = 26;
+
+function Digit() {
+
+  this.x;
+  this.y;
+  this.val;
+
+  this.an = 2;
+  this.al = 10;
+  this.sep = 1;
+  this.anT = -2;
+  this.alT = -2;
+  this.onCol = color(255, 0, 0);
+  this.offCol = 20;
+  this.sepDigCan = 1;
+  this.digCan = this.an*2+this.al*2+this.anT+this.sep*2+this.sepDigCan;
+
+  this.display = function() {
+    this.xM = this.x;
+    this.yM = this.y;
+    this.xUL = this.x-this.al-this.sep-this.anT;
+    this.yUL = this.y-this.al-this.sep-this.alT;
+    this.xU = this.x;
+    this.yU = this.y-this.al*2-this.sep*2-this.alT*2;
+    this.xUR = this.x+this.al+this.sep+this.anT;
+    this.yUR = this.y-this.al-this.sep-this.alT;
+    this.xDR = this.x+this.al+this.sep+this.anT;
+    this.yDR = this.y+this.al+this.sep+this.alT;
+    this.xD = this.x;
+    this.yD = this.y+this.al*2+this.sep*2+this.alT*2;
+    this.xDL = this.x-this.al-this.sep-this.anT;
+    this.yDL = this.y+this.al+this.sep+this.alT;
+
+    // middle (horizontal) M
+    if (this.val == 0 || this.val == 1 || this.val == 7 || this.val == null) {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xM-this.al-this.anT, this.yM);
+      vertex(this.xM-this.al-this.anT+this.an, this.yM-this.an);
+      vertex(this.xM+this.al+this.anT-this.an, this.yM-this.an);
+      vertex(this.xM+this.al+this.anT, this.yM);
+      vertex(this.xM+this.al+this.anT-this.an, this.yM+this.an);
+      vertex(this.xM-this.al-this.anT+this.an, this.yM+this.an);
+    endShape(CLOSE);
+
+    // up left segment (vertical) UL
+    if (this.val == 1 || this.val == 2 || this.val == 3 || this.val == 7 ||
+       this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xUL, this.yUL-this.al-this.alT);
+      vertex(this.xUL+this.an, this.yUL-this.al+this.an-this.alT);
+      vertex(this.xUL+this.an, this.yUL+this.al-this.an+this.alT);
+      vertex(this.xUL, this.yUL+this.al+this.alT);
+      vertex(this.xUL-this.an, this.yUL+this.al-this.an+this.alT);
+      vertex(this.xUL-this.an, this.yUL-this.al+this.an-this.alT);
+    endShape(CLOSE);
+
+    // up segment (horizontal) U
+    if (this.val == 1 || this.val == 4 || this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xU-this.al-this.anT, this.yU);
+      vertex(this.xU-this.al-this.anT+this.an, this.yU-this.an);
+      vertex(this.xU+this.al+this.anT-this.an, this.yU-this.an);
+      vertex(this.xU+this.al+this.anT, this.yU);
+      vertex(this.xU+this.al+this.anT-this.an, this.yU+this.an);
+      vertex(this.xU-this.al-this.anT+this.an, this.yU+this.an);
+    endShape(CLOSE);
+
+    // up right segment (vertical) UD
+    if (this.val == 5 || this.val == 6 || this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xUR, this.yUR-this.al-this.alT);
+      vertex(this.xUR+this.an, this.yUR-this.al+this.an-this.alT);
+      vertex(this.xUR+this.an, this.yUR+this.al-this.an+this.alT);
+      vertex(this.xUR, this.yUR+this.al+this.alT);
+      vertex(this.xUR-this.an, this.yUR+this.al-this.an+this.alT);
+      vertex(this.xUR-this.an, this.yUR-this.al+this.an-this.alT);
+    endShape(CLOSE);
+
+    // down right segment (vertical) DR
+    if (this.val == 2 || this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xDR, this.yDR-this.al-this.alT);
+      vertex(this.xDR+this.an, this.yDR-this.al+this.an-this.alT);
+      vertex(this.xDR+this.an, this.yDR+this.al-this.an+this.alT);
+      vertex(this.xDR, this.yDR+this.al+this.alT);
+      vertex(this.xDR-this.an, this.yDR+this.al-this.an+this.alT);
+      vertex(this.xDR-this.an, this.yDR-this.al+this.an-this.alT);
+    endShape(CLOSE);
+
+    // down segment (horizontal) D
+    if (this.val == 1 || this.val == 4 || this.val == 7 || this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xD-this.al-this.anT, this.yD);
+      vertex(this.xD-this.al-this.anT+this.an, this.yD-this.an);
+      vertex(this.xD+this.al+this.anT-this.an, this.yD-this.an);
+      vertex(this.xD+this.al+this.anT, this.yD);
+      vertex(this.xD+this.al+this.anT-this.an, this.yD+this.an);
+      vertex(this.xD-this.al-this.anT+this.an, this.yD+this.an);
+    endShape(CLOSE);
+
+    // down left segment (vertical) DL
+    if (this.val == 1 || this.val == 3 || this.val == 4 || this.val == 5 ||
+      this.val == 7 || this.val == 9 || this.val == null || this.val == "-") {
+      fill(this.offCol);
+    } else {
+      fill(this.onCol);
+    }
+    beginShape();
+      vertex(this.xDL, this.yDL-this.al-this.alT);
+      vertex(this.xDL+this.an, this.yDL-this.al+this.an-this.alT);
+      vertex(this.xDL+this.an, this.yDL+this.al-this.an+this.alT);
+      vertex(this.xDL, this.yDL+this.al+this.alT);
+      vertex(this.xDL-this.an, this.yDL+this.al-this.an+this.alT);
+      vertex(this.xDL-this.an, this.yDL-this.al+this.an-this.alT);
+    endShape(CLOSE);
+
+  }
+}
+function Separator() {
+  this.x=200;
+  this.y=10;
+  this.onCol = 255;
+  this.display = function() {
+    this.al = 8;
+    this.up = this.y-this.al;
+    this.down = this.y+this.al;
+    fill(this.onCol);
+    ellipse(this.x, this.up, this.al/2)
+    ellipse(this.x, this.down, this.al/2)
+  };
+}
